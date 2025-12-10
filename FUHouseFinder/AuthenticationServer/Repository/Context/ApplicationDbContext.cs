7843e91b1e62b5c546047bba89d19efcd566fcf5
Phạm Gia Khánh <phamgiakhanh0709@gmail.com>
2025-12-10 20:08:25 +0700

    Add initial API and Authentication server projects

---
+++ FUHouseFinder/AuthenticationServer/Repository/Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationServer.Repository.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            // Configure your entities here
+        }
+    }
+}
