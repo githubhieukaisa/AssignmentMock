db86a9914a130a8fed1f37a1b9d0a1f0cc9cb69d
NgHuy197 <141827178+NgHuy197@users.noreply.github.com>
2025-12-13 22:22:07 +0700

    Add rate feature
    add IRateRepository, RateRepository, IRateService, RateService, RateController

---
+++ FU_House_Finder/Repositories/IRateRepository.cs
@@ -0,0 +1,10 @@
+﻿using FU_House_Finder.Repositories.Models;
+
+namespace FU_House_Finder.Repositories
+{
+    public interface IRateRepository
+    {
+        Task<IEnumerable<Rate>> GetRatesByHouseIdAsync(int houseId);
+        Task<Rate> CreateRateAsync(Rate rate);
+    }
+}
